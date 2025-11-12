0239002abd492cb62808b4b028060abfd3faaa07
gab-kyoko <gabriellekyoko@alunos.utfpr.edu.br>
2025-11-11 23:12:21 -0300

    Add tipo_hotel.h with room types and locations

---
+++ Backend/tipo_hotel.h
@@ -0,0 +1,22 @@
+#ifndef TIPOS_HOTEL_H
+#define TIPOS_HOTEL_H
+
+// Enumeração para tipos de quartos disponíveis
+enum class TipoQuarto
+{
+    S, // Solteiro
+    D, // Duplo
+    C, // Casal
+    T, // Triplo
+    Q  // Quádruplo
+};
+
+// Enumeração para localidades dos hotéis
+enum class Localidade
+{
+    JERICOACOARA,
+    CANOA_QUEBRADA,
+    CUMBUCO
+};
+
+#endif
